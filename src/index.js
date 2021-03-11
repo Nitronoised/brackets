b9ad96aaeb2146954e28eafe0f94568987035005
Yaseen Al-Hashimi <55101943+Nitronoised@users.noreply.github.com>
2021-03-11 12:50:29 +0200

    init : add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+  let sample = config.reduce((a, b) => a + b).split("").filter(e => e !== ',');
+  let original = str.split("");
+  let mutual = [];
+  for (let i = 0; i < sample.length; i++) {
+    original.map((e) => e == sample[i] && mutual.push(true))
+  }
+  return (mutual.length >= sample.length) ? true : false
 }
